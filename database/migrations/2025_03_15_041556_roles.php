dc4378dbd9e1284e81c7db41d18f1f6bf31aa700
Jonatan Eduardo Ramirez Lopez <yonylpz01@hotmail.com>
2025-03-14 22:27:19 -0600

    creando el modelo, controller, migracion de users

--- database/migrations/2025_03_15_041556_roles.php
+++ database/migrations/2025_03_15_041556_roles.php
@@ -6,19 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
-        //
+        /*Inicio configuración*/
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id(); // Auto Increment y Primary Key
+            $table->string('nombre', 100); // VARCHAR(100)
+            $table->timestamps(); // Crea columnas created_at y updated_at
+        });
+
+        /*Fin Configuración*/
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
-        //
+        /*Inicio configuracion*/
+
+        Schema::dropIfExists('users'); // Elimina la tabla si existe
+
+        /*Fin Configuracion */
     }
 };
